982e3cdd5b04255109f4e6b33f3162ee80b82ad1
Ahmed Ibrahim <a.ibrahim@itqansystems.com>
2021-02-09 04:35:34 +0200

    init

---
+++ src/Client/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mzk1MjM5QDMxMzgyZTM0MmUzMEIrSHdpRGpBU0xNTlhxSjdFM1hTTHl0M1FzTm9iWDF1cnFmaXlTOXVXNlU9");
+
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSyncfusionBlazor();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
