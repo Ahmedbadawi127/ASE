15d83f3d6fd2de2b018c61168a643db2945d371c
Ahmed Ibrahim <a.ibrahim@itqansystems.com>
2021-06-04 21:39:27 +0200

    no finished

---
+++ src/Shared/Commands/FileUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Shared.Commands
+{
+    public static class FileUtil
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+            => js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+    }
+}
